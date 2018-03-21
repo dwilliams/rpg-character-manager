412df1357bda22adabeef173f1ddb88b8e2b3a86
Daniel Williams <dwilliams@port8080.net>
2018-03-21 16:55:00 -0600

    Adding classes in modules.

---
+++ char_sheet.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+### IMPORT ###
+import logging
+import pygame
+
+### GLOBALS ###
+
+### FUNCTIONS ###
+
+### CLASSES ###
+class CMCharSheetOne:
+    def __init__(self):
+        self.logger = logging.getLogger(type(self).__name__)
+
+    def draw(self):
+        self.logger.debug("Starting drawing")
+
+### MAIN ###
+if __name__ == '__main__':
+    pass
