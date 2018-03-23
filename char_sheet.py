50571724761a7d3ad7d98ace3ccbc1a317c0a839
Daniel Williams <dwilliams@port8080.net>
2018-03-22 18:49:33 -0600

    Trying to make the display bits resize.

--- char_sheet.py
+++ char_sheet.py
@@ -9,30 +9,76 @@ import pygame
 ### FUNCTIONS ###
 
 ### CLASSES ###
-class CMCharSheetOne:
-    def __init__(self, width, height):
+class CMCharSheet:
+    def __init__(self):
         self.logger = logging.getLogger(type(self).__name__)
         self.logger.debug("Initializing")
 
-        self.width = width
-        self.height = height
-        self.surface = pygame.Surface((self.width, self.height)) # Make a surface to blit everything down.
+        self.width = 0
+        self.height = 0
+        self.surface = None
 
         self.sections = [] # Sections of the sheet.  Add tuples with section objects and rects for placement.
                            # Placements are tuples with x, y locations
 
-        self.sections.append((object(), (0, 0)))
+        self.sections.append((CharSheetField(), (0, 0)))
 
-    def draw(self):
+    def draw(self, width, height):
         self.logger.debug("Starting drawing")
+
+        if self.width != width or self.height != height:
+            self.width = width
+            self.height = height
+            self.surface = pygame.Surface((self.width, self.height))
 
         for section in self.sections:
             self.logger.debug("Drawing section %s at placement %s", section[0], section[1])
             # Need to add isDirty logic here
             # section.draw stuff here
             # self.surface.blit stuff here too
+            tmp_surface = section[0].draw(width, height)
+            self.surface.blit(tmp_surface, section[1])
 
         return self.surface
+
+class CharSheetField:
+    def __init__(self):
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.debug("Initializing")
+
+        self.width = 0
+        self.height = 0
+        self.surface = None
+        
+        self.label = "Label"
+        self.value = "Value"
+        self.color_background = pygame.Color('white')
+        self.color_border = pygame.Color('black')
+        self.color_thickness = 1
+        self.isDirty = True
+
+    def draw(self, width, height):
+        self.logger.debug("Starting drawing")
+
+        if self.width != width or self.height != height:
+            self.width = width
+            self.height = height
+            self.isDirty = True
+            self.surface = pygame.Surface((self.width, self.height))
+
+        if self.isDirty:
+            self.surface.fill(self.color_background)
+            pygame.draw.rect(self.surface, self.color_border, pygame.Rect(0, 0, self.width, self.height), self.color_thickness)
+            #self.isDirty = False
+
+        return self.surface
+
+    def update(self, value):
+        self.value = value
+        self.isDirty = True
+
+    def mark_dirty(self):
+        self.isDirty = True
 
 ### MAIN ###
 if __name__ == '__main__':
--- run.py
+++ run.py
@@ -24,22 +24,22 @@ def main():
 
     # Init Values
     logging.debug("Initializing Values")
-    screen_size = screen_width, screen_height = 320, 240
+    screen_size = screen_width, screen_height = 850 // 2, 1100 // 2
     frame_rate = 30
     stop_event_loop = False
     color_background = pygame.Color('blue')
 
     # Setup screen, et. al.
     logging.debug("Setting up Engine Components")
-    screen = pygame.display.set_mode(screen_size)
+    screen = pygame.display.set_mode(screen_size, pygame.RESIZABLE)
     frame_clock = pygame.time.Clock()
 
     # Setup assets
     logging.debug("Setting up Assets")
-    font_default_large = pygame.font.Font(None, 36)
+    font_default_fps = pygame.font.Font(None, 20)
 
     ### Test Asset ###
-    cmcharsheetone = char_sheet.CMCharSheetOne(100, 100)
+    cmcharsheetone = char_sheet.CMCharSheet()
 
     # Event Loop
     logging.debug("Starting Event Loop")
@@ -50,21 +50,25 @@ def main():
             logging.debug("Event Type: %s (%d)", pygame.event.event_name(event.type), event.type)
             if event.type == pygame.QUIT:
                 stop_event_loop = True
+            if event.type == pygame.VIDEORESIZE:
+                screen_size = screen_width, screen_height = event.w, event.h
+                screen = pygame.display.set_mode(screen_size, pygame.RESIZABLE)
 
         # Do more stuff, e.g. draw
         text = "FPS: {}".format(frame_clock.get_fps())
-        text_width, text_height = font_default_large.size(text)
-        surface_fps = font_default_large.render(text, True, pygame.Color('red'), color_background)
+        #text_width, text_height = font_default_fps.size(text)
+        surface_fps = font_default_fps.render(text, True, pygame.Color('red'), color_background)
 
         ### Test Asset ###
-        cm_surface = cmcharsheetone.draw()
+        cm_surface = cmcharsheetone.draw(screen_width, screen_height - 20)
 
         # Fill, Blit, and Flip the screen
         logging.debug("Filling, Blitting, and Flipping")
         screen.fill(color_background)
 
         # // makes integer division in python3
-        screen.blit(surface_fps, ((screen_width - text_width) // 2, (screen_height - text_height) // 2))
+        screen.blit(surface_fps, (10, screen_height - 20))
+        screen.blit(cm_surface, (0, 0))
 
         pygame.display.flip()
 
