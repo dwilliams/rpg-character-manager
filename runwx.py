a6e254ac7d1e3c624e76d949b748c734b7e186f0
Daniel Williams <dwilliams@port8080.net>
2018-03-29 16:53:04 -0600

    Adding logging and starting to get things into the listctrl.

--- runwx.py
+++ runwx.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 ### IMPORTS ###
+import logging
 import wx
 
 ### GLOBALS ###
@@ -12,76 +13,106 @@ class HelloFrame(wx.Frame):
     def __init__(self, *args, **kwargs):
         # Ensure the parent's __init__ is called
         super().__init__(*args, **kwargs)
-        
+
+        # Setup logging for the class
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.debug("Initializing")
+
+        # Create an image list to be used in the listctrl
+        self.logolist = wx.ImageList()
+        #self.logolist.Add( wx.Bitmap(??))
+
         # Create a panel in the frame
         hf_panel = wx.Panel(self)
-        
+
         # Put stuff in
         #st = wx.StaticText(hf_panel, label="Hello World!", pos=(25, 25))
         #font = st.GetFont()
         #font.PointSize = font.PointSize + 10
         #font = font.Bold()
         #st.SetFont(font)
-        
+
         self.makeMenuBar()
-        
+
         self.CreateStatusBar()
         self.SetStatusText("Welcome to wxPython!")
-        
+
         # Overall sizers
         sizer1 = wx.BoxSizer(wx.HORIZONTAL)
         sizer2 = wx.BoxSizer(wx.VERTICAL)
         sizer3 = wx.BoxSizer(wx.VERTICAL)
         sizer1.Add(sizer2, 0, 0, 0)
         sizer2.Add(sizer3, 1, wx.EXPAND | wx.ALL, 1)
-        
+
         # Char list
-        listctrl1 = wx.ListCtrl(self, size = wx.Size(250, 800))
+        listctrl1 = wx.ListCtrl(self, size = wx.Size(250, 600))
         sizer2.Add(listctrl1, 0, 0, 0)
-        
+
         # Buttons below list
         sizer4 = wx.BoxSizer(wx.HORIZONTAL)
         button1 = wx.Button(self, label = "Add Char")
+        self.Bind(wx.EVT_BUTTON, self.OnAddButton, button1)
         button2 = wx.Button(self, label = "Del Char")
+        self.Bind(wx.EVT_BUTTON, self.OnDelButton, button2)
         sizer4.Add(button1, 0, 0, 0)
         sizer4.Add(button2, 0, 0, 0)
         sizer2.Add(sizer4, 0, 0, 0)
-        
+
         self.SetSizerAndFit(sizer1)
-    
+
     def makeMenuBar(self):
+        self.logger.debug("makeMenuBar")
         # Make a file menu with hello and exit items
         fileMenu = wx.Menu()
         helloItem = fileMenu.Append(-1, "&Hello...\tCtrl-H", "Help string shown in status bar for this menu item")
         fileMenu.AppendSeparator()
         exitItem = fileMenu.Append(wx.ID_EXIT)
-        
+
         # Make a help menu with an about item
         helpMenu = wx.Menu()
         aboutItem = helpMenu.Append(wx.ID_ABOUT)
-        
+
         # Make a menubar and add the menus
         menuBar = wx.MenuBar()
         menuBar.Append(fileMenu, "&File")
         menuBar.Append(helpMenu, "&Help")
         self.SetMenuBar(menuBar)
-        
+
         # Associate handler functions
         self.Bind(wx.EVT_MENU, self.OnHello, helloItem)
         self.Bind(wx.EVT_MENU, self.OnExit, exitItem)
         self.Bind(wx.EVT_MENU, self.OnAbout, aboutItem)
-    
+
     def OnExit(self, event):
+        self.logger.debug("OnExit")
         self.Close(True)
-    
+
     def OnHello(self, event):
+        self.logger.debug("OnHello")
         wx.MessageBox("Hello again from wxPython")
-    
+
     def OnAbout(self, event):
+        self.logger.debug("OnAbout")
         wx.MessageBox("This is a wxPython Hello World sample", "About Hello World 2", wx.OK | wx.ICON_INFORMATION)
+
+    def OnAddButton(self, event):
+        self.logger.debug("OnAddButton")
+        # Open a dialog asking for game type, char name, etc
+        # Gather the info from the dialog
+        # Create a new listctrl item
+        pass
+
+    def OnDelButton(self, event):
+        self.logger.debug("OnDelButton")
+        # Remove the selected character from the listctrl
+        pass
 
 class DerivedApp(wx.App):
     def OnInit(self):
+        # Setup logging for the class
+        self.logger = logging.getLogger(type(self).__name__)
+        self.logger.debug("Initializing")
+
         # Initialization and creation of the base/first frame
         the_frame = HelloFrame(None, title = "Hello World 2")
         the_frame.Show(True)
@@ -89,6 +120,9 @@ class DerivedApp(wx.App):
 
 ### MAIN ###
 def main():
+    # Init Logging
+    logging.basicConfig(level=logging.DEBUG)
+
     app = DerivedApp()
     app.MainLoop()
 
