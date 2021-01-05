edba43ab829f4d2c9d0ed09071401ffc87ec0e7d
Daniel Williams <dwilliams@port8080.net>
2021-01-05 16:34:21 -0700

    Need some level data too.  This and the ability data should probably go into a json file.  Maybe later.

---
+++ game_system/adnd/data_level.py
@@ -0,0 +1,124 @@
+DATA_LEVEL = {
+    'rogue': {
+        1: {
+            "experience": 0,
+            "thac0": 20,
+            "hitdice": 1,
+            "levelhp": 0
+        },
+        2: {
+            "experience": 1250,
+            "thac0": 20,
+            "hitdice": 2,
+            "levelhp": 0
+        },
+        3: {
+            "experience": 2500,
+            "thac0": 19,
+            "hitdice": 3,
+            "levelhp": 0
+        },
+        4: {
+            "experience": 5000,
+            "thac0": 19,
+            "hitdice": 4,
+            "levelhp": 0
+        },
+        5: {
+            "experience": 10000,
+            "thac0": 18,
+            "hitdice": 5,
+            "levelhp": 0
+        },
+        6: {
+            "experience": 20000,
+            "thac0": 18,
+            "hitdice": 6,
+            "levelhp": 0
+        },
+        7: {
+            "experience": 40000,
+            "thac0": 17,
+            "hitdice": 7,
+            "levelhp": 0
+        },
+        8: {
+            "experience": 70000,
+            "thac0": 17,
+            "hitdice": 8,
+            "levelhp": 0
+        },
+        9: {
+            "experience": 110000,
+            "thac0": 16,
+            "hitdice": 9,
+            "levelhp": 0
+        },
+        10: {
+            "experience": 160000,
+            "thac0": 16,
+            "hitdice": 10,
+            "levelhp": 0
+        },
+        11: {
+            "experience": 220000,
+            "thac0": 15,
+            "hitdice": 10,
+            "levelhp": 2
+        },
+        12: {
+            "experience": 440000,
+            "thac0": 15,
+            "hitdice": 10,
+            "levelhp": 4
+        },
+        13: {
+            "experience": 660000,
+            "thac0": 14,
+            "hitdice": 10,
+            "levelhp": 6
+        },
+        14: {
+            "experience": 880000,
+            "thac0": 14,
+            "hitdice": 10,
+            "levelhp": 8
+        },
+        15: {
+            "experience": 1100000,
+            "thac0": 13,
+            "hitdice": 10,
+            "levelhp": 10
+        },
+        16: {
+            "experience": 1320000,
+            "thac0": 13,
+            "hitdice": 10,
+            "levelhp": 12
+        },
+        17: {
+            "experience": 1540000,
+            "thac0": 12,
+            "hitdice": 10,
+            "levelhp": 14
+        },
+        18: {
+            "experience": 1760000,
+            "thac0": 12,
+            "hitdice": 10,
+            "levelhp": 16
+        },
+        19: {
+            "experience": 1980000,
+            "thac0": 11,
+            "hitdice": 10,
+            "levelhp": 18
+        },
+        20: {
+            "experience": 2200000,
+            "thac0": 11,
+            "hitdice": 10,
+            "levelhp": 20
+        }
+    }
+}
